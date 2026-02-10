152cb25cf56bdf4b280c2278066403f6e68a4dd2
abaddpnPC <thaithai9785@gmail.com>
2026-02-10 19:50:24 +0700

    package

--- src/tools/Acceptable.java
+++ src/tools/Acceptable.java
@@ -1,21 +1,19 @@
-
 package tools;
 
-public class Acceptable {
+public interface Acceptable {
+
     public final String TOUR_ID_VALID = "^[Tt]\\d{4}&";
     public final String HOME_ID_VALID = "^HS\\d{4}&";
     public final String INTEGER_VALID = "^[1-9]\\d*&";
-    
+
     public final String NAME_VALID = "^.+&";
     public final String TIME_VALID = "^.+&";
     public final String DATE_VALID = "^\\d{2}\\d{2}\\d{4}&";
-    
+
     public final String BOOKING_ID_VALID = "^B\\d{5}&";
     public final String PHONE_VALID = "^0\\d{9}&";
 
-    public static boolean isValid(String data, String pattern){
+    public static boolean isValid(String data, String pattern) {
         return data.matches(pattern);
     }
-
 }
-
